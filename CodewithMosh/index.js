192fba7c0124846eac52520a37702ab79d05f866
Saeed Iqbal <saeediqbal@Saeeds-MacBook-Pro.local>
2025-02-20 10:48:22 +0500

    Progress

---
+++ CodewithMosh/index.js
@@ -0,0 +1,30 @@
+
+console.log('Hello world');
+
+
+// Data Primitive Data types 
+let num = 10; // Number data type
+let name = 'saeed'; // String data type
+let isToggle = true; // Boolean ( ON / OFF )
+let lastName = undefined; // Undifiend Data type
+let choice = null;  //  Null - use when nothing to select
+
+
+
+
+
+// Objects
+
+let personDetails = {
+
+
+    name : 'saeed',
+    age: 24,
+    isMale : true
+}
+
+ //personDetails['isMale'] = "Male";
+
+ personDetails.isMale = "Male";
+ console.log(personDetails.isMale);
+ 
--- Datatypes/Numbers.js
+++ Datatypes/Numbers.js
@@ -1,1 +1,27 @@
-// number data type in js
+/* number data type in js
+ Number data type in js is used to represnt int and float. 
+ Used for mathematic calculations
+
+
+*/
+
+
+// Simple number
+let x= 500;
+console.log(x);
+
+// Arthemetic Operations
+let a=5;
+let b=6;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+
+// Special Methods
+
+console.log(typeof(a));
+console.log()
+
